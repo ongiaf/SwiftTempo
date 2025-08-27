a6d59c1143afdad35aa3588a3145506f6b2b43d1
ongiaf <147675548+ongiaf@users.noreply.github.com>
2025-08-28 00:37:34 +0800

    Initial Commit

---
+++ SwiftTempo/SwiftTempoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTempoApp.swift
+//  SwiftTempo
+//
+//  Created by Clarke on 2025/8/28.
+//
+
+import SwiftUI
+
+@main
+struct SwiftTempoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftTempoTests/SwiftTempoTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftTempoTests.swift
+//  SwiftTempoTests
+//
+//  Created by Clarke on 2025/8/28.
+//
+
+import Testing
+@testable import SwiftTempo
+
+struct SwiftTempoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
